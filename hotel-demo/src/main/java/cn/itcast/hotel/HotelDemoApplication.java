b7eb75a8fc8f5f7714e46f84c208d97266207e33
wangjin115 <1150553544@qq.com>
2024-05-10 23:39:55 +0900

    酒店搜索

--- hotel-demo/src/main/java/cn/itcast/hotel/HotelDemoApplication.java
+++ hotel-demo/src/main/java/cn/itcast/hotel/HotelDemoApplication.java
@@ -3,6 +3,9 @@ package cn.itcast.hotel;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,4 +19,10 @@ public class HotelDemoApplication {
         SpringApplication.run(HotelDemoApplication.class, args);
     }
 
+    @Bean
+    public RestHighLevelClient client(){
+       return new RestHighLevelClient(RestClient.builder(
+                HttpHost.create("http://192.168.90.128:9200")
+        ));
+    }
 }
